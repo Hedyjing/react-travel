4fd85af252a74b2953950bb8b3eed5d2c4198d6f
Hedyjing <374884275@qq.com>
2021-09-24 23:17:18 +0800

    use react-router配置主页路由

--- README.md
+++ README.md
@@ -24,3 +24,17 @@
 -productCollection内部的ProductImage小组件
 ## 主页合作企业组件
 - BusinessPartners
+## 路由与SPA
+### 路由
+react没有路由系统, 需要引入专门的路由框架react-router
+
+## 配置react-router
+react-router和react-router-dom
+- 安装react-router-dom会自动安装react-router核心框架
+- <Link /> 组件可以渲染出<a/>标签
+- <BrowserRouter /> 组件利用H5 API实现路由器切换
+- <HashRouter /> 组件利用原生js中的window.location.hash实现路由切换
+1. 把主页移植到page中, 定义为class组件
+2. 安装react-router-dom,同时安装框架ts定义支持
+3. 如何知道某个第三方原生支不支持ts呢, 使用了就知道了
+4. 用BrowserRouter组件配置主页路由
--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,17 @@
 import React from 'react';
 import styles from './App.module.css';
-import { Header, Footer, SideMenu, Carousel, ProductCollection, BusinessPartners } from './components';
-import { Row, Col, Typography } from 'antd';
-import { productList1, productList2, productList3 } from './mockups'
+import { BrowserRouter, Route } from 'react-router-dom';
+import {HomePage} from './pages'
 
-import sideImage1 from './assets/images/sider_2019_12-09.png';
-import sideImage2 from './assets/images/sider_2019_02-04.png';
-import sideImage3 from './assets/images/sider_2019_02-04-2.png';
 
-import {companies} from './assets'
 
 function App() {
   return (
     <div className={styles.App}>
-      <Header></Header>
-      {/* 页面内容content */}
-      <div className={styles['page-content']}>
-        <Row style={{ marginTop: 20 }}>
-          <Col span={6}>
-            <SideMenu />
-          </Col>
-          <Col span={18}>
-            <Carousel />
-          </Col>
-        </Row>
-        <ProductCollection
-          title={<Typography.Title level={3} type='warning'>爆款推荐</Typography.Title>}
-          sideImage={sideImage1}
-          product={productList1}
-        />
-        <ProductCollection
-          title={<Typography.Title level={3} type='danger'>新品上市</Typography.Title>}
-          sideImage={sideImage2}
-          product={productList2}
-        />
-        <ProductCollection
-          title={<Typography.Title level={3} type='success'>国内游推荐</Typography.Title>}
-          sideImage={sideImage3}
-          product={productList3}
-        />
-        <BusinessPartners companies={companies} />
-      </div>
-      <Footer></Footer>
+      <BrowserRouter>
+        <Route path='/' component={HomePage} />
+      </BrowserRouter>
+
     </div>
   );
 }
